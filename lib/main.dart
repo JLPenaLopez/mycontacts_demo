ef14fb1bc561e1f87e88621cb5eca0eb2726f8da
Jorge Luis Peña Lopez <jlp7490@gmail.com>
2019-05-31 15:50:18 -0500

    - Se elimina codigo innecesario - Se quitar banner de mode debug en la parte superior derecha - Se cambia color de la app a rojo

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        // primaryColor: Colors.yellow
       ),
       // home: MyHomePage(title: 'Flutter Demo Home Page'),
       home: HomePage("Mis Notas")
--- lib/src/pages/HomePage.dart
+++ lib/src/pages/HomePage.dart
@@ -8,32 +8,18 @@ class HomePage extends StatefulWidget {
 
   @override
   _HomePageState createState() {
-    // TODO: implement createState
     return _HomePageState();
   }
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Widget> _list = [
-    ListTile(
-      title: Text("Hola"),
-    ),
-    Divider(),
-    ListTile(
-      title: Text("Hola"),
-    ),
-    Divider(),
-    ListTile(
-      title: Text("Hola"),
-    ),
-    Divider(),
-  ];
 
   List<String> listS = List<String>();
 
   Widget _listItems() {
     if (listS.length > 0) {
       return ListView.builder(
+        padding: EdgeInsets.all(10),
         itemCount: listS.length,
         itemBuilder: (context, i) {
           final name = listS[i];
@@ -52,68 +38,18 @@ class _HomePageState extends State<HomePage> {
         child: Text("No hay contactos agregados"),
       );
     }
-    //List list = List<Widget>();
-    // for (int i = 0; i <= 5; i++) {
-    //   final ListTile tile = ListTile(
-    //     title: Text('Hola Mundo $i'),
-    //   );
-    //   _list.add(tile);
-    //   if (i < 50) {
-    //     _list.add(Divider());
-    //   }
-    // }
-
-    // return ListView(
-    //   children: <Widget>[
-    //     Column(
-    //       children: <Widget>[
-    //         ListTile(
-    //           title: Text("Hola"),
-    //           subtitle: Text("Subtitulo"),
-    //           leading: Icon(Icons.add_location),
-    //           contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-    //         ),
-    //         Divider()
-    //       ],
-    //     )
-    //   ],
-    // );
-
-    // return ListView(
-    //   // children: _list.toList(),
-    //   children: _list.toList(),
-    //   addAutomaticKeepAlives: false,
-    // );
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.titleHome),
       ),
-      // body: Builder(
-      //   builder: (context) => Center(
-      //         // child: RaisedButton(
-      //         //   onPressed: () {
-      //         //     print("Hola Mundo");
-      //         //     final snackBar = SnackBar(
-      //         //       content: Text("Hola!"),
-      //         //     );
-      //         //     Scaffold.of(context).showSnackBar(snackBar);
-      //         //   },
-      //         //   child: Text("Button"),
-      //         // ),
-      //         child: _listItems(),
-      //       ),
-      // ),
       body: _listItems(),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          // listS.add("Hola Mundo");
-          // setState(() {});
           final result = await Navigator.push(context,
               MaterialPageRoute(builder: (context) {
             return Contact();
